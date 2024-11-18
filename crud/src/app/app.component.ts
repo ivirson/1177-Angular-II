6b8ca9f6eb704dc3bdd85de5885a9ce1f1f7a648
Ivirson Daltro <ivi.daltro@gmail.com>
2024-11-18 20:30:49 -0300

    Aula 02 - Rotas

--- crud/src/app/app.component.ts
+++ crud/src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './common/header/header.component';
 import { RegisterComponent } from './modules/auth/components/register/register.component';
+import { ProductsCreateComponent } from './modules/products/components/products-create/products-create.component';
+import { ProductsListComponent } from './modules/products/components/products-list/products-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RegisterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    RegisterComponent,
+    ProductsListComponent,
+    ProductsCreateComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ crud/src/app/modules/products/components/products-create/products-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-create.component.html',
+  styleUrl: './products-create.component.css'
+})
+export class ProductsCreateComponent {
+
+}
