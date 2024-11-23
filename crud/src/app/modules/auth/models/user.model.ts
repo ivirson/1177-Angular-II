785388535be10630f2f24f8c7f10e970f3ed8e8c
Ivirson Daltro <ivi.daltro@gmail.com>
2024-11-22 22:02:21 -0300

    Aula 03 - User interface

--- crud/src/app/modules/auth/models/user.model.ts
+++ crud/src/app/modules/auth/models/user.model.ts
@@ -3,8 +3,18 @@ export interface User {
   profession: string;
   birthDate: string;
   documentNumber: string;
+  phone: string;
   email: string;
   password: string;
-  phone: string;
-  income: number;
+  address: Address;
+}
+
+export interface Address {
+  zipCode: string;
+  street: string;
+  number: number;
+  complement: string;
+  neighborhood: string;
+  city: string;
+  state: string;
 }
